5259e46864335a4dc506c8f4283fb5f35a4fa5b7
code1218 <skjil1218@gmail.com>
2025-11-27 17:38:28 +0900

    간단 todo

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import App01 from './ch01/App01';
 import App02 from './ch02/App02';
 import Counter from './ch02/Counter';
 import App02_2 from './ch02/App02_2';
+import App02_3 from './ch02/App02_3';
 
 const appObj = {
     ch01: <App01 />,
     ch02: <App02 />,
     ch02_2: <App02_2 />,
+    ch02_3: <App02_3 />,
     counter: <Counter />,
 }
 
-createRoot(document.getElementById('root')).render(appObj.ch02_2);
+createRoot(document.getElementById('root')).render(appObj.ch02_3);
