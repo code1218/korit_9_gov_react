0c20afd8b24c7664ffdd16582c9d43c58e283095
code1218 <skjil1218@gmail.com>
2025-11-27 11:52:31 +0900

    useState

--- src/ch02/App02.jsx
+++ src/ch02/App02.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
 import "./style.css";
 
 function App02() {
-    console.log("메인 박스")
-    return <div className="main-box"></div>
+    const [ name, setName ] = useState("기본 이름");
+
+    const data = {
+        title: "리액트 기초 시작",
+        nameValue: "기본 이름",
+        age: "0",
+    }
+
+    const handleNameInputOnChange = (event) => {
+        console.log(event.target.value);
+        setName(event.target.value);
+    }
+
+    return <div className="main-box">
+        <div className="title-box">
+            <h1>제목: {data.title}</h1>
+        </div>
+        <div className="input-box">
+            <input type="text" value={name} onChange={handleNameInputOnChange} />
+            <input type="text" value={data.age} />
+        </div>
+        <div className="button-box">
+            <button>확인</button>
+            <button>초기화</button>
+        </div>
+        <div className="display-box">
+            <ul>
+                <li>이름: {name}</li>
+                <li>나이: </li>
+            </ul>
+        </div>
+    </div>
 }
 
 export default App02;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App01 from './ch01/App01';
 import App02 from './ch02/App02';
+import Counter from './ch02/Counter';
 
 const appObj = {
     ch01: <App01 />,
     ch02: <App02 />,
+    counter: <Counter />,
 }
 
 createRoot(document.getElementById('root')).render(appObj.ch02);
