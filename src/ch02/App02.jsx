1a24fd299ed7a6102f9fc3ac41fbd3f1826a8170
code1218 <skjil1218@gmail.com>
2025-11-27 12:42:40 +0900

    input useState

--- src/ch02/App02.jsx
+++ src/ch02/App02.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import "./style.css";
 
 function App02() {
-    const [ name, setName ] = useState("기본 이름");
+    const [ name, setName ] = useState("기본 이름");     // input의 value 값이 변하면
+    const [ name2, setName2 ] = useState("");           // 확인 버튼이 눌러지면
+    const [ age, setAge ] = useState("");
+    const [ age2, setAge2 ] = useState("");
 
     const data = {
         title: "리액트 기초 시작",
@@ -11,8 +14,27 @@ function App02() {
     }
 
     const handleNameInputOnChange = (event) => {
-        console.log(event.target.value);
         setName(event.target.value);
+    }
+
+    const handleAgeInputOnChange = (event) => {
+        setAge(event.target.value);
+    }
+
+    //handleOkOnClick
+
+    const handleOkOnClick = () => {
+        setName2(name);
+        setAge2(age);
+        setName("");
+        setAge("");
+    }
+
+    const handleResetOnClick = () => {
+        setName2("");
+        setAge2("");
+        setName("");
+        setAge("");
     }
 
     return <div className="main-box">
@@ -21,16 +43,16 @@ function App02() {
         </div>
         <div className="input-box">
             <input type="text" value={name} onChange={handleNameInputOnChange} />
-            <input type="text" value={data.age} />
+            <input type="text" value={age} onChange={handleAgeInputOnChange} />
         </div>
         <div className="button-box">
-            <button>확인</button>
-            <button>초기화</button>
+            <button onClick={handleOkOnClick}>확인</button>
+            <button onClick={handleResetOnClick}>초기화</button>
         </div>
         <div className="display-box">
             <ul>
-                <li>이름: {name}</li>
-                <li>나이: </li>
+                <li>이름: {name2}</li>
+                <li>나이: {age2}</li>
             </ul>
         </div>
     </div>
