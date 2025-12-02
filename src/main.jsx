b0db72de31d0d9a33a6914176355422e889021c9
code1218 <skjil1218@gmail.com>
2025-12-02 11:53:21 +0900

    keydown net

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import UseEffect04 from './ch03/UseEffect04';
 import Axios01 from './ch04/Axios01';
 import Promise01 from './ch04/Promise01';
 import Axios02 from './ch04/Axios02';
+import Axios03 from './ch04/Axios03';
 
 const appObj = {
     ch01: <App01 />,
@@ -28,7 +29,8 @@ const appObj = {
     useEffect04: <UseEffect04 />,
     axios01: <Axios01 />,
     axios02: <Axios02 />,
+    axios03: <Axios03 />,
     promise01: <Promise01 />,
 }
 
-createRoot(document.getElementById('root')).render(appObj.axios02);
+createRoot(document.getElementById('root')).render(appObj.axios03);
