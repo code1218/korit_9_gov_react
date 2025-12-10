f7cd8231257bffc54f04d1981825dc3028bf75b9
code1218 <skjil1218@gmail.com>
2025-12-10 14:50:55 +0900

    router

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,11 @@ import Axios04 from './ch04/Axios04';
 import Axios05 from './ch04/Axios05';
 import Auth01 from './ch05/Auth01';
 import Auth02 from './ch05/Auth02';
+import Router01 from './ch06/Router01';
+import Router02 from './ch06/Router02';
+import Router03 from './ch06/Router03';
+import Router04 from './ch06/Router04';
+import Router05 from './ch06/Router05';
 
 const appObj = {
     ch01: <App01 />,
@@ -39,6 +44,11 @@ const appObj = {
     promise01: <Promise01 />,
     auth01: <Auth01 />,
     auth02: <Auth02 />,
+    router01: <Router01 />,
+    router02: <Router02 />,
+    router03: <Router03 />,
+    router04: <Router04 />,
+    router05: <Router05 />,
 }
 
-createRoot(document.getElementById('root')).render(appObj.auth02);
+createRoot(document.getElementById('root')).render(appObj.router05);
